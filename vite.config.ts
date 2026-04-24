815552e10da7ba6c88849270193482bf58c06dee
hadumon <145672032+hadumon@users.noreply.github.com>
2026-04-24 08:26:48 +0545

    feat: add path alias configuration in Vite for improved module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import path from "path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   server: {
     host: "0.0.0.0",
     port: 8080,
